8a0c658daca47521fd66bba03a4eda5c9aa4ede7
rubyncreates <rubyncreates@gmail.com>
2026-04-26 15:56:11 +0930

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,23 +2,23 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
-  
-module.exports = function(eleventyConfig) {
+
   // Copy assets folder to output
   eleventyConfig.addPassthroughCopy("src/assets");
 
   // Copy admin folder (Decap CMS) to output
   eleventyConfig.addPassthroughCopy("src/admin");
-  
+
   // Copy robots.txt to output
-eleventyConfig.addPassthroughCopy("src/robots.txt");
-  
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+
   // Format dates nicely
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(dateObj).toLocaleDateString('en-AU', options);
   });
-// Get all unique tags across all posts
+
+  // Get all unique tags across all posts
   eleventyConfig.addFilter("getAllTags", (collections) => {
     const tagSet = new Set();
     for (const item of collections.posts || []) {
@@ -26,6 +26,7 @@ eleventyConfig.addPassthroughCopy("src/robots.txt");
     }
     return Array.from(tagSet).sort();
   });
+
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return new Date(dateObj).toISOString().split('T')[0];
   });
